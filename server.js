d0ad446b7211f997d1452127348d24f1713caf4d
Bakhtier Gaibulloev <slashmsu@gmail.com>
2016-12-06 21:13:06 +0100

    allow all types requests

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose.connect(secret.database, function(err) {
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
-    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
     res.header('Access-Control-Max-Age', '1000');
     next();
